d2e28f46a15d606a602477abace0f2a04792c353
Bilol <sharipovbilolx777@gmail.com>
2026-07-28 23:47:11 +0500

    Web app va kanal tugmasi qo'shildi

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+TOKEN = "7993407351:AAEXWz5AmSl0HyPyOgO8VxkBEavKFGYpiIU"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()  # Mana shu qator yozilgan bo'lishi shart!
 @dp.message(Command("kanalga_tashash"))
+
 async def send_to_channel(message: Message):
     web_app_keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
